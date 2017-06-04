afe2a760bcbfe196c3ad4ee038a501ca46dc255d
Mikel Olasagasti <mikel@olasagasti.info>
2017-06-04 14:48:45 +0200

    FluxBB esteka kendu

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
             <div class="clear"></div>
             <div id="oina">
                 <p id="informazio-menua"><a href="<?php echo get_option('home'); ?>/wiki/Zer da Librezale?" title="<?php _e('What is Librezale?', 'librezale') ?>"><?php _e('What is Librezale?', 'librezale') ?></a> | <a href="<?php echo get_option('home'); ?>/wiki/Kontaktua" title="<?php _e('Contact Information', 'librezale') ?>"><?php _e('Contact', 'librezale') ?></a> | <a href="<?php echo get_option('home'); ?>/wiki/RSS jarioa" title="<?php _e('RSS Feed', 'librezale') ?>"><?php _e('RSS Feed', 'librezale') ?></a> | <a href="<?php echo get_option('home'); ?>/wiki/Lizentzia" title="<?php _e('License', 'librezale') ?>"><?php _e('License', 'librezale') ?></a></p>
-                <p id="kreditu-menua"><strong><?php _e('Software Used:', 'librezale') ?></strong> <a href="http://www.mediawiki.org" title="MediaWiki">MediaWiki</a>, <a href="http://eu.wordpress.org" title="WordPress">WordPress</a> eta <a href="http://www.fluxbb.org" title="FluxBB">FluxBB</a></p>
+                <p id="kreditu-menua"><strong><?php _e('Software Used:', 'librezale') ?></strong> <a href="http://www.mediawiki.org" title="MediaWiki">MediaWiki</a> eta <a href="http://eu.wordpress.org" title="WordPress">WordPress</a></p>
             </div>
         </div>
 
